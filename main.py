e148b642ce16215cfc2d56703de82232149770c3
hibiki <hibiki123123@gmail.com>
2021-08-14 07:16:56 +0900

    all

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write("hello")
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
